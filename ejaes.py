c77f344dbdebebddba0d7eb3fe970cf0716e6cce
Stefano Tomasini Hoefner <seomasini@gmail.com>
2023-04-27 16:10:19 +0200

    agregué opción para encriptar

--- ejaes.py
+++ ejaes.py
@@ -1,23 +1,14 @@
 
-# ¡¡IMPORTANTE!!
+# ¡¡¡IMPORTANTE!!!
 # Primero ejecutar en terminal "pip install pycryptodome"
 
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
-from Crypto.Random import get_random_bytes
 
 key = b'SeguridadInforma'
 iv = key
 # la b es para poner la cadena en formato de array de bytes, para que pueda ser utilizada como clave AES
-# Vector de inicialización igual a la clave
-
-def aes_encrypt(plaintext):
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    padded_plaintext = pad(plaintext.encode('utf-8'), AES.block_size)
-    # el "padding" consiste en agregar bytes de relleno al mensaje para que este tenga un nº de bytes que sea múltiplo de 16
-    # sino, no se podría aplicar el algoritmo de cifrado en bloques de 128 bits (16 bytes)
-    ciphertext = cipher.encrypt(padded_plaintext)
-    return ciphertext
+# Vector de inicialización pseudoaleatorio de 16 bytes, mismo tamaño que la clave
 
 def aes_decrypt(ciphertext):
     cipher = AES.new(key, AES.MODE_CBC, iv)
@@ -26,10 +17,22 @@ def aes_decrypt(ciphertext):
     # el unpad elimina los bytes de relleno del mensaje que se habían agregado anteriormente al cifrar el mensaje
     return unpadded_plaintext.decode('utf-8')
 
+def aes_encrypt(plaintext):
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    padded_plaintext = pad(plaintext.encode('latin-1'), AES.block_size)
+    ciphertext = cipher.encrypt(padded_plaintext)
+    return ciphertext.hex()
 
-ciphertext = input().strip()
+print("Presione 1 para desencriptar, 2 para encriptar")
 
-ciphertext_as_array_of_bites = bytes.fromhex(ciphertext)
+decision = int(input().strip())
 
-decrypted_plaintext = aes_decrypt(ciphertext_as_array_of_bites)
-print("Mensaje desencriptado:", decrypted_plaintext)
+if decision == 1:
+    print("introduzca string hexadecimal a desencriptar: ")
+    print("String desencriptado:", aes_decrypt(bytes.fromhex(input().strip())))
+else:
+    print("introduzca string a encriptar: ")
+    print("String encriptado:", aes_encrypt(input().strip()))
+
+# ¡¡¡IMPORTANTE!!!
+# Primero ejecutar en terminal "pip install pycryptodome"
